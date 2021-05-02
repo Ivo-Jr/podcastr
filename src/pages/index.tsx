3fe1ebb285d8138e0921f47ef7355604722deb67
IvoJr <maciel.jr95@gmail.com>
2021-05-02 11:02:36 -0300

    included typescript and axios

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,22 @@
-export default function Home(props) {
+/* 
+ * O SSG funciona apenas em ambiente de produção. Por isso é necessário biuldar;
+ * "yarn start" -> Projeto roda na mesma forma que está em produção. (Tudo funcional no NEXT);
+ * "yarn dev" -> Projeto em desenvolvimento.
+*/
+import { GetStaticProps } from 'next';
+import { api } from '../services/api';
+
+type Episode = {
+  id: string;
+  title: string;
+  members: string;
+}
+
+type HomeProps = {
+  episodes: Episode[];
+}
+
+export default function Home(props: HomeProps) {
   return (
     <div>
       <h1>Index</h1>
@@ -7,9 +25,14 @@ export default function Home(props) {
   )
 }
 
-export async function getStaticProps() {
-  const response = await fetch('http://localhost:3333/episodes');
-  const data = await response.json()
+export const getStaticProps: GetStaticProps = async () => {
+  const { data } = await api.get('http://localhost:3333/episodes', {
+    params: {
+      _limit: 12,
+      _sort: 'published_at',
+      _order: 'desc',
+    }
+  });
 
   return {
     props: {
