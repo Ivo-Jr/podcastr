11d208e3589706ca1b4594e6b6da1f77ad421cc2
IvoJr <maciel.jr95@gmail.com>
2021-05-30 09:11:18 -0300

    gereacao de paginas estaticas dinamicas

--- src/pages/episodes/[slug].tsx
+++ src/pages/episodes/[slug].tsx
@@ -3,7 +3,7 @@ import Image from 'next/image';
 import ptBR from 'date-fns/locale/pt-BR'
 import Link from 'next/link';
 import { GetStaticPaths, GetStaticProps } from 'next';
-import { useRouter } from 'next/router'
+import { useRouter } from 'next/router';
 
 import { api } from '../../services/api';
 import { convertDurationToTimeString } from '../../utils/convertDurationToTimeString';
@@ -58,8 +58,25 @@ export default function Episode({ episode }: EpisodeProps) {
 }
 
 export const getStaticPaths: GetStaticPaths = async () => {
+  const { data } = await api.get('episodes', {
+    params: {
+      _limit: 2,
+      _sort: 'published_at',
+      _order: 'desc',
+    }
+  });
+
+
+  const paths = data.map(episode => {
+    return {
+      params: {
+        slug: episode.id
+      }
+    }
+  })
+
   return {
-    paths: [],
+    paths,
     fallback: 'blocking'
   }
 }
