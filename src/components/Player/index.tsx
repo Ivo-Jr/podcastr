d2a0c2e10f4d8d93d857e80fbd7b4ccd8acea58c
IvoJr <maciel.jr95@gmail.com>
2021-05-31 13:18:48 -0300

    starting with contexts

--- src/components/Player/index.tsx
+++ src/components/Player/index.tsx
@@ -1,11 +1,15 @@
+import { useContext } from 'react';
+import { PalyerContext } from '../../contexts/PlayerContexts';
 import styles from './styles.module.scss';
 
 export function Player() {
+  const player = useContext(PalyerContext)
+
   return (
     <div className={styles.playerContainer}>
       <header>
         <img src="/playing.svg" alt="Tocando agora" />
-        <strong>Tocando agora</strong>
+        <strong>Tocando agora {player}</strong>
       </header>
 
       <div className={styles.emptyPlayer}>
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,19 +2,23 @@ import '../styles/global.scss'
 
 import { Header } from '../components/Header';
 import { Player } from '../components/Player';
+import { PalyerContext } from '../contexts/PlayerContexts';
 
 
 import styles from '../styles/app.module.scss';
 
 function MyApp({ Component, pageProps }) {
   return (
-    <div className={styles.wrapper}>
-      <main>
-        <Header />
-        <Component {...pageProps} /> 
-      </main>
-      <Player />
-    </div>
+    <PalyerContext.Provider value={'Junior'}>
+      <div className={styles.wrapper}>
+        <main>
+          <Header />
+          <Component {...pageProps} /> 
+        </main>
+        <Player />
+      </div>
+    </PalyerContext.Provider>
+
   )
 }
 
