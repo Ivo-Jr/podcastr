7a85f9e9114441f407e5c3771cb847764438d11d
IvoJr <maciel.jr95@gmail.com>
2021-06-15 08:30:49 -0300

    controle pelo slider

--- src/components/Player/index.tsx
+++ src/components/Player/index.tsx
@@ -26,7 +26,8 @@ export function Player() {
     playPrevious,
     setPlayingState,
     hasPrevious,
-    hasNext
+    hasNext,
+    clearPlayerState
   } = usePlayer();
 
   useEffect(() => {
@@ -47,6 +48,19 @@ export function Player() {
     audioRef.current.addEventListener('timeupdate', () => {
       setProgress(Math.floor(audioRef.current.currentTime));
     });
+  }
+
+  function handleSeek(amount: number) {
+    audioRef.current.currentTime = amount;
+    setProgress(amount)
+  }
+
+  function handleEpisodeEnded() {
+    if (hasNext) {
+      playNext()
+    } else {
+      clearPlayerState();
+    }
   }
   
   const episode = episodeList[currentEpisodeIndex];
@@ -79,6 +93,7 @@ export function Player() {
               <Slider
                 max={episode.duration}
                 value={progress}
+                onChange={handleSeek}
                 trackStyle={{ backgroundColor: '#04d361' }}
                 railStyle={{ backgroundColor: '#9f75ff' }}
                 handleStyle={{ borderColor: '#04d361', borderWidth: 4 }}
@@ -96,6 +111,7 @@ export function Player() {
             ref={audioRef}
             loop={isLooping}
             autoPlay
+            onEnded={handleEpisodeEnded}
             onPlay={() => setPlayingState(true)}
             onPause={() => setPlayingState(false)}
             onLoadedMetadata={setupProgressListener}
--- src/contexts/PlayerContexts.tsx
+++ src/contexts/PlayerContexts.tsx
@@ -22,6 +22,7 @@ type PlayerContextData = {
   toggleShuffle: () => void;
   playNext: () => void;
   playPrevious: () => void;
+  clearPlayerState: () => void;
   hasPrevious: boolean;
   hasNext: boolean;
 };
@@ -68,8 +69,13 @@ export function PlayerContextProvider({ children }: PlayerContextProviderProps) 
     setIsPlaying(state)
   }
 
+  function clearPlayerState() {
+    setEpisodeList([]);
+    setCurrentEpisodeIndex(0);
+  }
+
   const hasPrevious = currentEpisodeIndex > 0;
-  const hasNext = (currentEpisodeIndex + 1) < episodeList.length;
+  const hasNext = isShuffling || (currentEpisodeIndex + 1) < episodeList.length;
 
 
   function playNext() {
@@ -105,7 +111,8 @@ export function PlayerContextProvider({ children }: PlayerContextProviderProps) 
         setPlayingState,
         playList,
         hasPrevious,
-        hasNext
+        hasNext,
+        clearPlayerState
         }}
       >
       {children}
